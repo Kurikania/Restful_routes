d532e235dd9f9bc11f37f82b964ff1736cae22d9
Kurikania <65830166+Kurikania@users.noreply.github.com>
2020-10-21 16:50:55 +0300

    add delete and edit routes

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express(); 
 let ejs = require('ejs'); 
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override')
 
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
-const comments = [
+app.use(methodOverride('_method'))
+let comments = [
     {
         id: uuidv4(), 
         username: "Josh", 
@@ -38,11 +40,33 @@ app.post('/comments', (req, res) =>{
     comments.push({username, description, id: uuidv4()});
     res.redirect("/comments");
 })
+
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params; 
     const comment = comments.find(c => c.id === id);
     res.render('show', {comment}); 
 })
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params; 
+    const comment = comments.find(c => c.id === id);
+    res.render('edit', {comment})
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params; 
+    const newCommentText = req.body.description; 
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.description = newCommentText; 
+    res.redirect('/comments')
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params; 
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments')
+})
+
 app.get('/tacos', (req,res) => {
     res.render('tacos')
 })
@@ -51,6 +75,8 @@ app.post('/tacos', (req,res) => {
     const {meat, qty} =  req.body
     res.send(`Ok you have ordered ${qty}  ${meat}`)
 })
+
+
 
 app.listen(3000, () => {
     console.log('on port 3000')
